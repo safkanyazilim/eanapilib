ef67e91dca793c75533855592fa6fe871c7263c6
Aaron Klor <aaron.klor@gmail.com>
2012-11-08 09:52:30 -0600

    #3 Missed some new chekcstyle problems caused by this fix.
    Fixed some other minor problems with the build.

---
+++ api-lib/src/app/com/ean/mobile/exception/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright 2012 EAN.com, L.P. All rights reserved.
+ */
+/**
+ * This package holds the exceptions that might be thrown by the various parts of this jar.
+ */
+package com.ean.mobile.exception;
