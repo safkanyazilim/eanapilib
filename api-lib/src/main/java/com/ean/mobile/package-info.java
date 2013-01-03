5b52797a21f456e3c8c757e5530ea60686bb2d10
Aaron Klor <aaron.klor@gmail.com>
2013-01-03 15:15:13 -0600

    #38 Updated the copyright on all of the files in the project.
    Added copyrights to the files who didn't have them.

--- api-lib/src/main/java/com/ean/mobile/package-info.java
+++ api-lib/src/main/java/com/ean/mobile/package-info.java
@@ -1,6 +1,7 @@
 /*
- * Copyright 2012 EAN.com, L.P. All rights reserved.
+ * Copyright 2013 EAN.com, L.P. All rights reserved.
  */
+
 /**
  * Classes in this package are used to interact with the java system and hold EAN data in java objects.
  */
--- api-lib/src/main/java/com/ean/mobile/request/package-info.java
+++ api-lib/src/main/java/com/ean/mobile/request/package-info.java
@@ -1,6 +1,7 @@
 /*
- * Copyright 2012 EAN.com, L.P. All rights reserved.
+ * Copyright 2013 EAN.com, L.P. All rights reserved.
  */
+
 /**
  * This package holds the concrete implementations and the abstract class for the requests to be performed to the
  * EAN API.
