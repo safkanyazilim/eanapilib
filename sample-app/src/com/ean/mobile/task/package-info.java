020dced9c19d72306c929f88223b8c11437c67d8
Aaron Klor <aaron.klor@gmail.com>
2013-01-23 14:32:03 -0600

    #76 Added package-info files
    356 errors remaining.

---
+++ sample-app/src/com/ean/mobile/task/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2013 EAN.com, L.P. All rights reserved.
+ */
+
+/**
+ * This holds all of the full classes that either are or interact with AsyncTask that are not part of a specific
+ * activity.
+ */
+package com.ean.mobile.task;
