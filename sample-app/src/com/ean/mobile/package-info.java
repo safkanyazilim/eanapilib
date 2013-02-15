b7ec0aba1f1248e979692d4689968d526271e898
Aaron Klor <aaron.klor@gmail.com>
2013-02-15 12:46:49 -0600

    #76 Missed some problems with the header checkstyle.

--- sample-app/src/com/ean/mobile/package-info.java
+++ sample-app/src/com/ean/mobile/package-info.java
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 2013 EAN.com, L.P. All rights reserved.
  */
- */
 
 /**
  *  This package holds all of the basic files for the app, including the Application class, and other helper classes.
--- sample-app/src/com/ean/mobile/task/package-info.java
+++ sample-app/src/com/ean/mobile/task/package-info.java
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 2013 EAN.com, L.P. All rights reserved.
  */
- */
 
 /**
  * This holds all of the full classes that either are or interact with AsyncTask that are not part of a specific
